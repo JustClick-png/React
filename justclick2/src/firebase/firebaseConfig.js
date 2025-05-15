e05bd2af7d5c5a7cb69a6f39ce11c3a94bcec536
anebeato <anebeato@gmail.com>
2025-05-15 12:16:15 +0200

    aaa

--- justclick2/src/firebase/firebaseConfig.js
+++ justclick2/src/firebase/firebaseConfig.js
@@ -1,32 +1,26 @@
 // Import the functions you need from the SDKs you need
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
-import { getStorage } from 'firebase/storage'; 
+import { getStorage } from "firebase/storage";
 
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCpHw_eBKkNOHt8U82HBDO_XCVUxMTTej8",
   authDomain: "tfg-almi-f0864.firebaseapp.com",
   projectId: "tfg-almi-f0864",
-  storageBucket: "tfg-almi-f0864.firebasestorage.app",
+  storageBucket: "tfg-almi-f0864.appspot.com", // ✅ CORREGIDO
   messagingSenderId: "447143105340",
   appId: "1:447143105340:web:931fe45f0941ae81c24439",
   measurementId: "G-SCEJYB176J"
 };
 
+// Inicialización
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app); 
+const db = getFirestore(app);
 const auth = getAuth(app);
 const analytics = getAnalytics(app);
 const storage = getStorage(app);
 
 export { db, auth, analytics, storage };
-
